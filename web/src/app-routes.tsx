385a8ee6d1691174b325d4c97657bb2a7688b0ec
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-08-11 12:49:22 -0300

    chore: add vercel.json

--- web/src/app-routes.tsx
+++ web/src/app-routes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { Home } from "./pages/home";
 import { Product } from "./pages/product";
+import { Cart } from "./pages/cart";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/product" element={<Product />} />
+      <Route path="/cart" element={<Cart />} />
     </Routes>
   );
 }
---
+++ web/src/pages/cart.tsx
@@ -0,0 +1,9 @@
+export function Cart() {
+  return (
+    <div>
+      <div className="max-w-[1040px] mx-auto my-16">
+        <h3>Carrinho</h3>
+      </div>
+    </div>
+  );
+}
