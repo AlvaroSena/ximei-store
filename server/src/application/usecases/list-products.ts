466639ed509d33de32fc3321ff002bc07ee96a53
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-08-14 11:10:34 -0300

    feat: add redis for caching products data

--- server/src/application/usecases/list-products.ts
+++ server/src/application/usecases/list-products.ts
@@ -1,4 +1,5 @@
 import { prisma } from "../../infra/prisma";
+import redis from "../../infra/redis";
 
 interface ListProductsRequest {
   page: number;
@@ -9,6 +10,14 @@ export class ListProducts {
   async execute({ page, perPage }: ListProductsRequest) {
     const skip = (page - 1) * perPage;
     const take = perPage;
+
+    const cacheKey = `products:page:${page}:perPage:${perPage}`;
+
+    const cachedProducts = await redis.get(cacheKey);
+
+    if (cachedProducts) {
+      return JSON.parse(cachedProducts);
+    }
 
     const [products, totalProducts] = await Promise.all([
       prisma.product.findMany({
@@ -23,11 +32,19 @@ export class ListProducts {
 
     const totalPages = Math.ceil(totalProducts / perPage);
 
-    return {
+    const result = {
       products,
       totalProducts,
       totalPages,
       currentPage: page,
     };
+
+    if (process.env.NODE_ENV === "production") {
+      await redis.set(cacheKey, JSON.stringify(result), { ex: 60 });
+    } else {
+      await redis.setEx(cacheKey, 60, JSON.stringify(result));
+    }
+
+    return result;
   }
 }
