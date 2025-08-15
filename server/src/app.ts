d2cb672a687494e35e8d952143c27f0577010a98
Alvaro Sena <alvarosenacs.c@gmail.com>
2025-08-14 21:03:25 -0300

    refactor: setup correct cors config

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,7 +5,12 @@ import { prisma } from "./infra/prisma";
 import { errorHandler } from "./infra/middlewares/error-handler";
 
 const app: Application = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL!,
+    methods: "*",
+  })
+);
 app.use(express.json());
 app.use(routes);
 
